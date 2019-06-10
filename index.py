fc2529948cab0006e8f772644e8f7fa628076c76
Emperor-1 <49476001+Emperor-1@users.noreply.github.com>
2019-06-10 11:31:09 +0800

    Update index.py
    目前还有counter函数未读懂，还有cascaded函数等一些零散的实现细节

--- index.py
+++ index.py
@@ -13,13 +13,13 @@ def counter(func):
         global lastsave
         if time.time() - lastsave > 3:
             # this is in seconds, so 5 minutes = 300 seconds
-            lastsave = time.time()
+            lastsave = time.time() # time 函数以秒为精度，获取当前时间，返回浮点类型
             tmp.count = 0
         return func(*args, **kwargs)
     tmp.count = 0
     return tmp
 
-#级联分类器，基于机器学习，通过大量的样本训练得到的分类器
+#级联分类器，基于机器学习，通过大量的样本训练得到的分类器，目前推测返回的是一个函数
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
@@ -29,14 +29,14 @@ cap = cv2.VideoCapture(0)
 
 @counter
 def closed():
-  print("Eye Closed")
+  print("Eye is Closed")
 
 
 def openeye():
   print("Eye is Open")
 
 
-
+#音频输出函数，可通过耳机口输出音乐
 def sound():
     mixer.init()
     mixer.music.load('sound.mp3')
@@ -53,12 +53,12 @@ while 1:
         roi_color = img[y:y + h, x:x + w]
 
         eyes = eye_cascade.detectMultiScale(roi_gray)
-
-        if eyes is not (): #is not 判断前后两个变量是否处于同一地址  （）表示空的元组类型
+#判断上一函数是否检测到了有效的eyes，若是则有值，若否则返回空的元组值
+        if eyes is not (): #is not() 判断前后两个变量是否处于同一地址   //  （）表示空的元组类型
             for (ex, ey, ew, eh) in eyes:
-                cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+                cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)  #框出眼睛
                 openeye()
-        else:
+        else:               #未检测到有效的eyes数据
            closed()
            if closed.count == 3:
                print("driver is sleeping")
